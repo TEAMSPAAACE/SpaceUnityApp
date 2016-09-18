71fa237068928106152efcb45e5d7481e03b0a05
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-18 05:56:56 -0700

    Five levers with sound

---
+++ Assets/Scripts/Environment/SliderAudio.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class SliderAudio : MonoBehaviour 
+{
+	private AudioSource sliderAudio;
+
+	void OnEnable()
+	{
+		sliderAudio = GetComponent<AudioSource>();
+		GameObject.FindGameObjectWithTag (Tags.SLIDER_TASK_2).GetComponentInChildren<VRTK_Control> ().defaultEvents.OnValueChanged.AddListener (SliderSound);
+	}
+
+	private void SliderSound()
+	{
+		if (sliderAudio.isPlaying) 
+		{
+			sliderAudio.Stop();
+		}
+
+		sliderAudio.Play();
+	}
+
+}
