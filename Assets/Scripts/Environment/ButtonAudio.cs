c5e60f5d4c5e6bd10dd626a078572474080f2c8a
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-18 04:43:39 -0700

    Add button noise on press, pre-fab 5 buttons

---
+++ Assets/Scripts/Environment/ButtonAudio.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class ButtonAudio : MonoBehaviour 
+{
+	private AudioSource buttonAudio;
+
+	void OnEnable()
+	{
+		buttonAudio = GetComponent<AudioSource>();
+		GameObject.FindGameObjectWithTag(Tags.BUTTON_TASK_1).GetComponentInChildren<VRTK_Button>().events.OnPush.AddListener(ButtonPressSound);
+	}
+
+	private void ButtonPressSound()
+	{
+		if (buttonAudio.isPlaying) 
+		{
+			buttonAudio.Stop();
+		}
+
+		buttonAudio.Play();
+	}
+
+}
